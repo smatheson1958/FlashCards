e0068cedaa88f4c61b124b7b7ba14fbb2e1d6d87
scottmatheson58 <scott@matheson.it>
2026-04-05 16:55:48 +0100

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by scott matheson on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
